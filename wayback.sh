894bdbf3fa3b38364fb73eddb3b9ce969e4ed0ef
Victor4X <viggo00@hotmail.com>
2020-03-28 16:53:26 +0100

    Added API comment

--- wayback.sh
+++ wayback.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#API here: https://archive.readme.io/docs/creating-a-snapshot
 
 wbID=`curl -s -X POST -H "Content-Type: application/json" -d '{"url": "'$1'", "annotation": {"id": "lst-ib", "message": ""}}' https://pragma.archivelab.org 2>&1 | jq '."wayback_id"'`
 trimmed="${wbID:1:-1}"
