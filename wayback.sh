ab0c224639b37b3515ee02dbff15cfd9f15ca08c
Victor4X <viggo00@hotmail.com>
2020-03-28 16:11:52 +0100

    Basic functionality

---
+++ wayback.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+wbID=`curl -s -X POST -H "Content-Type: application/json" -d '{"url": "'$1'", "annotation": {"id": "lst-ib", "message": ""}}' https://pragma.archivelab.org 2>&1 | jq '."wayback_id"'`
+trimmed="${wbID:1:-1}"
+echo "http://web.archive.org"$trimmed
